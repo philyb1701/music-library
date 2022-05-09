0abaf23e987ca953c1987b357730fba6399aad73
Phil Burkhill <philip.burkhill@gmail.com>
2022-05-09 21:41:34 +0100

    create artist done

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,5 +1,19 @@
 //Create a src/controllers/artist.js file and write a controller function which returns a 201 status code.
+const getDb = require('../services/db');
 
-exports.create = (req, res) => {
-  res.sendStatus(201);
+exports.create = async (req, res) => {
+  const db = await getDb();
+  const { name, genre } = req.body;
+
+  try {
+    await db.query(
+      `INSERT INTO Artist (name, genre) VALUES ('${name}', '${genre}')`
+    );
+
+    res.sendStatus(201);
+  } catch (err) {
+    res.sendStatus(500).json(err);
+  }
+
+  db.close();
 };
--- tests/artist-create.test.js
+++ tests/artist-create.test.js
@@ -2,9 +2,18 @@
 
 const { expect } = require('chai');
 const request = require('supertest');
+const getDb = require('../src/services/db');
 const app = require('../src/app');
 
 describe('Create artists', () => {
+  let db;
+  beforeEach(async () => (db = await getDb()));
+
+  afterEach(async () => {
+    await db.query('DELETE FROM Artist');
+    await db.close();
+  });
+
   describe('/artist', () => {
     describe('POST', () => {
       it('creates a new artist in the database', async () => {
@@ -14,6 +23,13 @@ describe('Create artists', () => {
         });
 
         expect(res.status).to.equal(201);
+
+        const [[artistEntries]] = await db.query(
+          `SELECT * FROM Artist WHERE name = 'Tame Impala'`
+        );
+
+        expect(artistEntries.name).to.equal('Tame Impala');
+        expect(artistEntries.genre).to.equal('rock');
       });
     });
   });
