67504ff8290205da0f4b09967d77a04f5fd8521a
Phil Burkhill <philip.burkhill@gmail.com>
2022-05-11 19:54:53 +0100

    fixed controllers

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -4,15 +4,17 @@ const getDb = require('../services/db');
 exports.create = async (req, res) => {
   const db = await getDb();
   const { name, genre } = req.body;
+  //We put a ? at any point where we want to add a variable to our statement. Then we provide those variables in an array as the second argument to db.query(). mysql2 then automatically escapes any strings that are being passed into the statement, thereby preventing any malicious SQL being executed by the database.
 
   try {
-    await db.query(
-      `INSERT INTO Artist (name, genre) VALUES ('${name}', '${genre}')`
-    );
+    await db.query("INSERT INTO Artist (name, genre) VALUES (?, ?')", [
+      name,
+      genre,
+    ]);
+
     res.sendStatus(201);
   } catch (err) {
     res.sendStatus(500).json(err);
   }
-
   db.close();
 };
