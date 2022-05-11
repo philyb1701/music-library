be77dc6be1da2c7e5903fdc4a566c5d10712cda0
Phil Burkhill <philip.burkhill@gmail.com>
2022-05-11 20:32:36 +0100

    fixed tests

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -4,10 +4,9 @@ const getDb = require('../services/db');
 exports.create = async (req, res) => {
   const db = await getDb();
   const { name, genre } = req.body;
-  //We put a ? at any point where we want to add a variable to our statement. Then we provide those variables in an array as the second argument to db.query(). mysql2 then automatically escapes any strings that are being passed into the statement, thereby preventing any malicious SQL being executed by the database.
 
   try {
-    await db.query("INSERT INTO Artist (name, genre) VALUES (?, ?')", [
+    await db.query('INSERT INTO Artist (name, genre) VALUES (?, ?)', [
       name,
       genre,
     ]);
@@ -15,6 +14,18 @@ exports.create = async (req, res) => {
     res.sendStatus(201);
   } catch (err) {
     res.sendStatus(500).json(err);
+  }
+
+  db.close();
+};
+
+exports.read = async (_, res) => {
+  const db = await getDb();
+  try {
+    const [artists] = await db.query('SELECT * FROM Artist');
+    res.status(200).json(artists);
+  } catch (err) {
+    res.status(500).json(err);
   }
   db.close();
 };
--- src/routes/artist.js
+++ src/routes/artist.js
@@ -6,5 +6,6 @@ const artistController = require('../controllers/artist');
 const router = express.Router();
 
 router.post('/', artistController.create);
+router.get('/', artistController.read);
 
 module.exports = router;
